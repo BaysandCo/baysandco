a42cf89428db0a58bb29398af3876bc188be851b
Matthew Vilhauer <matthewvilhauer@gmail.com>
2019-09-18 21:52:57 -0400

    Updates

---
+++ precache-manifest.42e49265d7736cb2c10ae3e0843a967e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca9e1ce31c96770f3177f4321298e1c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5531ba1d6521e39da366",
+    "url": "/static/css/main.d70b5570.chunk.css"
+  },
+  {
+    "revision": "b685f1281385bd3deb85",
+    "url": "/static/js/2.8db52ba9.chunk.js"
+  },
+  {
+    "revision": "5531ba1d6521e39da366",
+    "url": "/static/js/main.251af4c3.chunk.js"
+  },
+  {
+    "revision": "e80fd076db4f79b25468",
+    "url": "/static/js/runtime~main.a57029d6.js"
+  }
+]);
