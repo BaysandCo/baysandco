f3a0eb1239d47a715b4256f848c7fdb6ecb23f38
Matthew Vilhauer <matthewvilhauer@gmail.com>
2019-09-22 13:46:53 -0400

    Updates

---
+++ precache-manifest.8a20db1d963a119bbbb73bc1a04a7261.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0dfdce63934ac12ec525d2fbdc8cd5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2f29261c06e7d9f2a50",
+    "url": "/static/css/main.407d919d.chunk.css"
+  },
+  {
+    "revision": "b685f1281385bd3deb85",
+    "url": "/static/js/2.8db52ba9.chunk.js"
+  },
+  {
+    "revision": "b2f29261c06e7d9f2a50",
+    "url": "/static/js/main.2bc3f28d.chunk.js"
+  },
+  {
+    "revision": "e80fd076db4f79b25468",
+    "url": "/static/js/runtime~main.a57029d6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/baysandco/precache-manifest.70a4e2e4a97b82e92f22d7aabc2a1ddc.js"
+  "/precache-manifest.8a20db1d963a119bbbb73bc1a04a7261.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/baysandco/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
